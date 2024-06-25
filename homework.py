1d17e40cdf527f32c1b08b432de4b18e6171ba70
DamirSul <_@example.com>
2024-06-26 01:19:46 +0600

    add changes

--- homework.py
+++ homework.py
@@ -63,6 +63,9 @@ logger.addHandler(handler)
 formatter = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
 handler.setFormatter(formatter)
 
+# Список выбрасываемых ошибок.
+error_messages = [] 
+
 
 def check_tokens():
     """Функция проверки токенов."""
@@ -94,69 +97,70 @@ def send_message(bot, message):
             f'Сообщение "{message}" успешно '
             f"отправлено в чат {TELEGRAM_CHAT_ID}"
         )
+        return True
     except ApiTelegramException as error:
         logger.error(f"Ошибка при отправке сообщения: {error}")
+        return False
 
 
 def get_api_answer(timestamp):
     """Функция-обработчик эндпоинта."""
     try:
-        response = requests.get(
-            ENDPOINT, headers=HEADERS, params={"from_date": timestamp}
-        )
+        response = requests.get(ENDPOINT, headers=HEADERS, params={"from_date": timestamp})
         response.raise_for_status()
     except requests.RequestException as e:
-        logger.error(f"{error_api_request} {e}")
-        send_message(TELEGRAM_CHAT_ID, f"{error_api_request} {e}")
-        raise ApiErrorException(f"{error_api_request} {e}")
+        error_message = f"{error_api_request} {e}"
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise ApiErrorException(error_message)
 
     if response.status_code != HTTPStatus.OK:
-        logger.error(f"{endpoint_is_not_available}{response.status_code}")
-        send_message(
-            TELEGRAM_CHAT_ID,
-            f"{endpoint_is_not_available}{response.status_code}",
-        )
-        raise NotAvailableEndPointException(
-            f"{endpoint_is_not_available}{response.status_code}"
-        )
+        error_message = f"{endpoint_is_not_available}{response.status_code}"
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise NotAvailableEndPointException(error_message)
 
     try:
         return response.json()
     except ValueError as e:
-        logger.error(f"{error_api_to_json}{e}")
-        send_message(TELEGRAM_CHAT_ID, f"{error_api_to_json}{e}")
-        raise ApiErrorException(f"{error_api_to_json}{e}")
-
+        error_message = f"{error_api_to_json}{e}"
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise ApiErrorException(error_message)
 
 def check_response(response):
     """Функция валидации ответа из функции get_api_answer."""
     if not isinstance(response, dict):
-        send_message(TELEGRAM_CHAT_ID, text=api_is_not_dict)
-        logging.error(api_is_not_dict)
-        raise TypeError(api_is_not_dict)
+        error_message = api_is_not_dict
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise TypeError(error_message)
     if "homeworks" not in response or "current_date" not in response:
-        send_message(TELEGRAM_CHAT_ID, text=empty_api_keys)
-        logging.error(empty_api_keys)
-        raise KeyError(empty_api_keys)
+        error_message = empty_api_keys
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise KeyError(error_message)
     if not isinstance(response["homeworks"], list):
-        send_message(TELEGRAM_CHAT_ID, text=hmwrks_is_not_list)
-        logging.error(hmwrks_is_not_list)
-        raise TypeError(hmwrks_is_not_list)
+        error_message = hmwrks_is_not_list
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise TypeError(error_message)
     return response
-
 
 def parse_status(homework):
     """Функция обновления статуса работы."""
     if "homework_name" not in homework or "status" not in homework:
-        logger.error(empty_hmwrks_keys)
-        send_message(TELEGRAM_CHAT_ID, empty_hmwrks_keys)
-        raise KeyError(empty_hmwrks_keys)
+        error_message = empty_hmwrks_keys
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise KeyError(error_message)
     homework_name = homework["homework_name"]
     status = homework["status"]
     if status not in HOMEWORK_VERDICTS:
-        logger.error(f"{not_doc_status} {status}")
-        send_message(TELEGRAM_CHAT_ID, f"{not_doc_status} {status}")
-        raise ValueError(f"{not_doc_status} {status}")
+        error_message = f"{not_doc_status} {status}"
+        logger.error(error_message)
+        error_messages.append(error_message)
+        raise ValueError(error_message)
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -191,20 +195,29 @@ def main():
         except Exception as error:
             message = f"Сбой в работе программы: {error}"
             logger.error(message)
+            error_messages.append(message)
 
             current_time = time.time()
             if (
                 message != last_error_message
                 or current_time - last_error_time > RETRY_PERIOD_ERROR_TIME
             ):
-                send_message(bot, message)
-                last_error_message = message
-                last_error_time = current_time
-            else:
-                logger.debug(
-                    "Анти-спам обнаружение. Сообщение не отправлено."
-                )
+                if send_message(bot, message):
+                    last_error_message = message
+                    last_error_time = current_time
+                else:
+                    logger.debug(
+                        "Анти-спам обнаружение. Сообщение не отправлено."
+                    )
         finally:
+            try:
+                for error_message in error_messages:
+                    if send_message(bot, error_message):
+                        error_messages.remove(error_message)
+
+            except Exception as e:
+                logger.critical(f"Невозможно отправить сообщение об ошибки пользователю: {e}.")
+
             time.sleep(RETRY_PERIOD)
 
 
